e046b1806cddcffd6dce9f37257202244ae6372a
LuisCarri <luiscarrillo@ciencias.unam.mx>
2025-05-18 22:52:48 -0600

    Ahora las tareas se guardan en un arreglo; se añadió un botón para agregar tareas, aún no tiene funcionalidad

--- app-notas/src/App.jsx
+++ app-notas/src/App.jsx
@@ -36,15 +36,36 @@ function Task({ title }) {
   );
 }
 
-function App() {
+function ListOfTasks({ tasks }) {
   return (
     <div>
-      <Task title="Comprar huevos" />
-      <Task title="Comprar leche" />
-      <Task title="Comprar pan" />
-      <Task title="Comprar frutas" />
-      <Task title="Comprar verduras" />
-      <Task title="Comprar carne" />
+      {tasks.map((task) => (
+        <Task key={task.id} title={task.title} />
+      ))}
+    </div>
+  );
+}
+
+function handleAddTask() {
+  console.log("Agregar tarea");
+}
+
+function App() {
+  let tasks = [
+    { id: 1, title: "Comprar huevos" },
+    { id: 2, title: "Comprar leche" },
+    { id: 3, title: "Comprar pan" },
+    { id: 4, title: "Comprar frutas" },
+    { id: 5, title: "Comprar verduras" },
+    { id: 6, title: "Comprar carne" },
+  ];
+
+  return (
+    <div className="App">
+      <ListOfTasks tasks={tasks} />
+      <Button onClick={handleAddTask}>
+        <p>Agregar tarea</p>
+      </Button>
     </div>
   );
 }
