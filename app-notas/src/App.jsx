3edc50d125b7c3b3d8f43149dc59d126d4a8a346
LuisCarri <luiscarrillo@ciencias.unam.mx>
2025-05-19 23:40:32 -0600

    Se añadió que las listas completadas se renderizen aparte, solo falta eliminarlas del otro lugar y arreglar un problema de renderizado

--- app-notas/src/App.jsx
+++ app-notas/src/App.jsx
@@ -2,78 +2,97 @@ import { useState } from "react";
 import "./App.css";
 
 function Button({ children, onClick }) {
-  return (
-    <button className="btn" onClick={onClick}>
-      {children}
-    </button>
-  );
+    return (
+        <button className="btn" onClick={onClick}>
+            {children}
+        </button>
+    );
 }
 
 function Input({ label }) {
-  return (
-    <>
-      <label htmlFor="input">{label}</label>
-      <input type="text" id="input" name="input" />
-      <input type="submit" value="Submit" />
-    </>
-  );
+    return (
+        <>
+            <label htmlFor="input">{label}</label>
+            <input type="text" id="input" name="input" />
+            <input type="submit" value="Submit" />
+        </>
+    );
 }
 
 function Card({ children }) {
-  return <div className="card">{children}</div>;
+    return <div className="card">{children}</div>;
 }
 
-function Task({ title }) {
-  const [isCompleted, setIsCompleted] = useState(false);
-  const handleMarkAsCompleted = () => {
-    if (!isCompleted) {
-      setIsCompleted(true);
-    }
-  };
-  return (
-    <Card>
-      {isCompleted ? (
-        <s>{title}</s>
-      ) : (
-        <>
-          {title}
-          <Button onClick={handleMarkAsCompleted}>
-            <p>Marcar tarea como completada</p>
-          </Button>
-        </>
-      )}
-    </Card>
-  );
+function Task({ title, isCompleted, onComplete }) {
+    return (
+        <Card>
+            {isCompleted ? (
+                <s>{title}</s>
+            ) : (
+                <>
+                    {title}
+                    <Button onClick={onComplete}>
+                        <p>Marcar tarea como completada</p>
+                    </Button>
+                </>
+            )}
+        </Card>
+    );
 }
 
-function ListOfTasks({ tasks }) {
-  return (
-    <div>
-      {tasks.map((task) => (
-        <Task key={task.id} title={task.title} />
-      ))}
-    </div>
-  );
+function ListOfTasks({ tasks, onComplete }) {
+    return (
+        <div>
+            {tasks.map((task) => (
+                <Task
+                    key={task.id}
+                    title={task.title}
+                    isCompleted={task.completed}
+                    onComplete={() => onComplete(task.id)}
+                />
+            ))}
+        </div>
+    );
 }
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
+    const [completedTasks, setCompletedTasks] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const inputValue = formData.get("input");
-    setTasks([...tasks, { id: tasks.length + 1, title: inputValue }]);
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const inputValue = formData.get("input");
+        setTasks([
+            ...tasks,
+            { id: tasks.length + 1, title: inputValue, completed: false },
+        ]);
+    };
 
-  return (
-    <div className="App">
-      <ListOfTasks tasks={tasks} />
-      <form onSubmit={handleSubmit}>
-        <Input label="Ingresa una nueva tarea:" />
-      </form>
-    </div>
-  );
+    const recoverCompletedTasks = () => {
+        const completed = tasks.filter((task) => task.completed);
+        setCompletedTasks(completed);
+    };
+
+    const markTaskAsCompleted = (id) => {
+        setTasks((tasks) =>
+            tasks.map((task) =>
+                task.id === id ? { ...task, completed: true } : task
+            )
+        );
+        recoverCompletedTasks();
+    };
+
+    return (
+        <div className="App">
+            <ListOfTasks tasks={tasks} onComplete={markTaskAsCompleted} />
+            <form onSubmit={handleSubmit}>
+                <Input label="Ingresa una nueva tarea:" />
+            </form>
+            <h1>Lista de tareas completadas</h1>
+            <ListOfTasks tasks={completedTasks} />
+        </div>
+    );
 }
 
 export default App;
